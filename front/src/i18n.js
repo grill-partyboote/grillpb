bd742a806a77fd201f6431a13dc602676d8d71fc
avoy <prey-88@yandex.ru>
2023-05-13 16:38:01 +0400

    i18n hints

--- front/src/i18n.js
+++ front/src/i18n.js
@@ -6,8 +6,10 @@ export function t(key){
 	return i18n[lang].key
 }
 
+// todo: take lang from browser api
 const lang = 'en';
 
+// here you can add translation
 const i18n = {
 	en: {
 		start: 'start',
