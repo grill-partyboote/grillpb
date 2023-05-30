be78e959f9d7458f4382703da5041080fde2bb5f
avoy <prey-88@yandex.ru>
2023-05-31 01:31:42 +0400

    remove excess todo

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,7 +4,6 @@ import {createRoot} from "react-dom/client";
 import "./styles.pcss";
 import { App } from "./App";
 
-// todo: add routing
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(<App/>);
